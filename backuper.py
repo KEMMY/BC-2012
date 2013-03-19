b935bc5ab71e08e14e463573817d4af22703af40
KEMMY <kemmy.patrik@gmail.com>
2013-03-19 09:57:09 +0100

    Update backuper.py

--- backuper.py
+++ backuper.py
@@ -13,29 +13,84 @@ class Target():
     def get_path():
         return self.target_path # volania napr. BackupObject.new...(... , target.get_path())
 
+
+
 class Backup():
-    def __init__(self,target, side_dict = None): # None sluzi len pre Backup.get_backup(time)
-        self.dict = side_dict # ???
-        self.time = self.get_time()
+    
+    @staticmethod
+    def _create_backup(source, target, backup_name):
+        if backup_name == None :
+            return NewBackup(source, target, backup_name)
+        elif backup_name != None : # sem by som este dal kontrolu formatu
+            return LatestBackup(source, target, backup_name)
+        else:
+            # Unknown Backup 
+            return None
+        
+    def __init__(self, source, target, backup_name = None):
+        self.time = self.get_time() # zbytocne ?!
+        self.source = source
         self.target = target
+        self.name = backup_name # napr. 2012-12-12T12:12 / v tedy sa pouzije namiesot self.time
+        
         
     def get_time(self):
         return datum.now().strftime('%Y-%m-%dT%H:%M:%S')
 
-    def make_backup(self):
+    def make_backup(self, time, side_dict):
         pickled_dict = pickle.dumps(self.dict)
-        file_name = self.target + "/backups/" + self.time
+        file_name = self.target + "/backups/" + time
         with open(file_name,"wb") as BF:
             BF.write(pickled_dict)
             BF.close()
-
+            
     def get_backup(self, time):
         file_name = self.target + "/backups/" + time
         with open(file_name, "rb") as BF:
             BF.read(load_dict)
             BF.close()
-        dict = pickle.loads(load_dict)
-        return dict['hash'] # vraciam hash korenoveho slovnika
+        side_dict = pickle.loads(load_dict)
+        return side_dict
+
+    def initial_backup(self):
+        # New / Last Backup
+        pass
+    
+    def incremental_backup(self):
+        # New / Latest Backup
+        pass
+    
+
+class NewBackup(Backup):
+    
+    def __init__(self, source, backup_name):
+        print "Initializing NewBackup"
+        print source
+        Backup.__init__(self, source, backup_name)
+
+    def initial_backup(self):
+        # vytvori novu zalohu
+        new_object = BackupObject.new_backup_object(self.source, self.target, self.target) # sem com chcel vsunut aj side_dict ako vstupny par. pri inite
+        side_dict = new_object.initial_backup()
+        self.make_backup(self.get_time(),side_dict) # self.get_time alebo self.time ?
+
+    def incremental_backup(self):
+        pass # todo
+
+
+class LatestBackUp(Backup):
+
+    def __init__(self, source, backup_name):
+        print "Initializing LatestBackup"
+        print source
+        Backup.__init__(self, source, backup_name)
+
+    def initial_backup(self):
+        pass #?
+    
+    def incremental_backup(self):
+        pass
+
     
 class BackupObject():
     
@@ -53,18 +108,24 @@ class BackupObject():
             return None
 
     @staticmethod
-    def new_backup_object(pathname, target):
-        self.stat = os.stat(pathname)
-        #self.hash = ...
-        # treba metody file_copy alebo make_hash dat do materskej triedy ?
-        #side_dict = self.make_side_dict(self.file_copy / self.make_hash)
-        return BackupObject._create_backup_object(pathname, target, side_dict)
+    def new_backup_object(pathname, target, side_dict):
+        stat = os.stat(pathname)
+        '''
+        self.pathname = pathname
+        self.target = target
+        self.side_dict = side_dict ak nieje none ... ak je None ziska ho pri Initial_backup ?
+        return BackupObject._create_backup_object(self.pathname, self.target, self.stat, self.side_dict)
+        MULTIPLICITA premennych ???
+
+        self. >>> @staticmethod nepouzivaju
+        '''
+        return BackupObject._create_backup_object( pathname, target,stat, side_dict)
 
     @staticmethod
     def existing_backup_object(pathname, target, side_dict):
-        stat = os.stat(pathname)
-        # hore ???
-        return BackupObject._create_backup_object(pathname, target, stat ,side_dict)
+        #stat = os.stat(pathname) >>> current stat netreba
+        stat = side_dict['stat'] # ale side_dict musi byt vitiahnuty zo zalohy/slovnika
+        return BackupObject._create_backup_object( pathname, target, stat ,side_dict)
 
     @staticmethod
     # existuje iba v zalohe
@@ -135,24 +196,33 @@ class BackupFile(BackupObject):
     def exist_backup(self):
         file_hash = self.make_hash(self.source)
         #print file_hash
-        for F in os.listdir(self.target): # co s sprazdnym dir prebehne cyklus ?
+        for F in os.listdir(self.target):
             if F == file_hash:
                 break
                 return True
         return False
+
+    
+    def compare_stat(self, object_stat, backuped_stat):
+        # pri zmene aspon jednej z mtime, uid, gid false inak true 
+        if object_stat.st_mtime == backuped_stat.st_mtime :
+            if object_stat.st_uid == backuped_stat.st_uid :
+                 if object_stat.st_gid == backuped_stat.st_gid :
+                     return True
+                 else : return False
+            else : return False
+        else : return False
+        
+    def find_file_name(self, name, dir_dict): # rekurzivny prechod slovnikov hlada meno suboru a potom porovna stat
+        file_dict = None
+        for file_name in dir_dict.iterkeys():
+            if file_name == name :
+                if self.compare_stat(self.side_dict , dir_dict[file_name]): # ak nasiel a su rovnake staty
+                    return dir_dict[file_name] .... file_copy update side_dict
+            else : False
+                
     
     def incremental_backup(self):
-        #print self.exist_backup()
-        #if self.exist_backup():
-            #print ('existuje')
-            #self.initial_backup()
-            #dopln do slovnik :
-            #self.source ziskaj base name
-            #hladaj base name v slovnikoch
-            #ak nasiel ak rodicov. adresar existuje a doplni
-            
-        #else:
-            #print ('nexistuje')
         pass
         
         
@@ -197,11 +267,10 @@ class BackupDir(BackupObject):
     
 
 class BackupLnk(BackupObject):
-    def __init__(self, pathname, target):
+    def __init__(self, pathname, target, side_dict):
         print "Initializing BackupLnk"
         print pathname
-        BackupObject.__init__(self, pathname,target)
-        pass
+        BackupObject.__init__(self, pathname, target, side_dict)
     
     def make_hash(self, src_file, block_size = constants.CONST_BLOCK_SIZE):
         file_hash = hashlib.sha1()
@@ -218,12 +287,14 @@ class BackupLnk(BackupObject):
     
     def make_Lnk(self, real_source):
         hash_name = self.make_hash(self.source)
-        os.symlink(real_source, self.target+'/objects/' + hash_name)
-
+        file_name = self.target + "/objects/" + hash_name
+        with open(file_name,"wb") as DF:
+            DF.write(real_source)
+        return hash_name
         
     def initial_backup(self):
-          # return self.make_side_dict(self.make_Lnk(self.read_Lnk()))
+        # return self.make_side_dict(self.make_Lnk(self.read_Lnk()))
         hash_name = self.make_Lnk(self.read_Lnk())
         return self.make_side_dict(hash_name)
+     
         
-
