fb02e3edbf7087c55020082347fd9ce46f7ab0af
KEMMY <kemmy.patrik@gmail.com>
2013-04-23 10:20:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-
 from backuper import *
 
-def initial_backup():
+def backup():
     print "class Target test"
     target = Target('/home/kmm/Plocha/target/new_target')
     print target.get_path()
@@ -10,15 +9,8 @@ def initial_backup():
     print ""
     back = NewBackup('/home/kmm/Plocha/source',target)
     print back.source_path
-    print back.name
-    print ""
-    print "///////////////////////TEST class Backup LATESTBACKUP////////////////////////"
-    print ""
-    latest = ExistingBackup('/home/kmm/Plocha/source',target,'2013-03-29T18:57:12')
-    print latest.source_path
-    print latest.name
     print "///////////////////TEST class NEWBACKUP initial_backup//////////////////////////"
-    back.initial_backup()
+    back.backup()
 
 
 def incremental_backup():
@@ -31,7 +23,7 @@ def incremental_backup():
     inc_back = ExistingBackup('/home/kmm/Plocha/source',target,'2013-04-11T23:56:07')
     print inc_back.source_path
     print inc_back.name
-    inc_back.incremental_backup()
+    inc_back.backup()
 
 def recovery_backup():
     print "//////////////////////class Target test//////////////////////"
@@ -40,16 +32,15 @@ def recovery_backup():
     print ""
     print "//////////////////////Start Recovery Backup/////////////////////////"
     print ""
-    rec_back = ExistingBackup('/home/kmm/Plocha/target/zaloha',target,'2013-04-11T23:56:07')
+    rec_back = ExistingBackup('/home/kmm/Plocha/target/zaloha',target,'2013-04-23T01:22:35') # treba vediet cas
     print rec_back.source_path
-    print rec_back.name
     rec_back.recovery_backup()
     
 def main():
     print "Hello"
-    #initial_backup()
+    backup()
     #incremental_backup()
-    recovery_backup()
+    #recovery_backup()
 
 if  __name__ == "__main__":
     main()
